c7d8b4fcb972dd97c262f5802f12d3aea847b93a
spprichard <spprichard20@gmail.com>
2018-05-08 18:39:39 -0500

    added print statement

--- Sources/STSLibrary/TensorFlowExample.swift
+++ Sources/STSLibrary/TensorFlowExample.swift
@@ -3,6 +3,7 @@ import TensorFlow
 struct TensorFlowExample {
     static func multiplyTensor() -> Tensor<Float> {
         let t = Tensor<Float>([1.2, 0.8])
+        print("hello...Anton")
         return t * t
     }
 }
